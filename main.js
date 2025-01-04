2cf6ca57680a193671cdd8c8dac409888c566c13
Hello-617-tech <brume617oghene@gmail.com>
2025-01-04 12:32:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -64,6 +64,7 @@ let move = 'idle';
 // Flag to track if the world has been loaded
 let worldLoaded = false;
 function loadWorld(){
+    glbLoader.setDRACOLoader(draco);
     glbLoader.load('./src/world.glb', function (gltf) {
         if (!worldLoaded) {
             gltf.scene.scale.set(100, 100, 100);
@@ -81,7 +82,6 @@ function loadWorld(){
 }
 loadWorld();
 function loadModel() {
-    fbxLoader.setDRACOLoader(draco);
     fbxLoader.load(
         './public/flame_boy/' + mode + '/' + move + '.fbx', // Dynamic file path
         (object) => {
